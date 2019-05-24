e61abd23460a04c515bb13048f21fa1169f05349
RadySonabu <ardynubanos@yahoo.com>
2019-05-25 02:18:35 +0800

    Nothing

---
+++ send_email.py
@@ -0,0 +1,27 @@
+import smtplib
+from email.message import EmailMessage
+
+
+EMAIL_ADDRESS = 'ardynubanos@gmail.com'
+EMAIL_PASSWORD = '<password>'
+contacts = ['ardynubanos@gmail.com', 'cziapco@gmail.com']
+
+msg = EmailMessage()
+msg['Subject'] = 'This should be a pdf'
+msg['From'] = EMAIL_ADDRESS
+msg['To'] = contacts
+
+files = ['something.html'] #folder directory
+
+for file in files:
+	with open(file, 'rb') as f:
+		file_data = f.read()
+		file_name = f.name
+
+	msg.add_attachment(file_data, maintype='application', subtype='octet-stream', filename=file_name)
+
+    
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        smtp.send_message(msg)
